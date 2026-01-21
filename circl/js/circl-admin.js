912325d70158ab83675077a88fc0b2a496e0e2ed
favstats <fabio.votta@gmail.com>
2026-01-21 16:31:54 +0100

    Update QC form: simplify text criteria, update Google Script URL

--- circl/js/circl-admin.js
+++ circl/js/circl-admin.js
@@ -18,7 +18,7 @@ let currentCommentPostId = null;
 let currentCommentPostData = null;
 
 const QC_CONFIG = {
-  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbzEK51M3YerARm4JgOW4JvXTnOGJYXUwV7pzGWgEg5KTclQYLVZQclClZHj5TPXVkImhQ/exec'
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbwncmUFSxJhUotk0v7iSwreKRM3_tbi_OHog9lFn5jAvxkNvGkG6u0zqU4nkG-TJyU8eA/exec'
 };
 
 const REACTIONS_CONFIG = {
